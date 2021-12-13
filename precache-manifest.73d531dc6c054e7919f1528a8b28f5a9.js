98ef371bef410961a421e1a0f86e97865cf1a3a6
shibapad <shibapadbsc@gmail.com>
2021-12-13 03:53:25 -0500

    Initial commit

---
+++ precache-manifest.73d531dc6c054e7919f1528a8b28f5a9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58b2dcd25d8568e5b79cee9bd583090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40e32023c478482902d",
+    "url": "/static/css/main.f28aa53d.chunk.css"
+  },
+  {
+    "revision": "2bb7c29ffa5624340d10",
+    "url": "/static/js/2.c7a1d434.chunk.js"
+  },
+  {
+    "revision": "8c5d97602b2de1b9706a14d21c0680ab",
+    "url": "/static/js/2.c7a1d434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e02de8ebbba571fb7f4",
+    "url": "/static/js/3.8113d922.chunk.js"
+  },
+  {
+    "revision": "b40e32023c478482902d",
+    "url": "/static/js/main.7704e800.chunk.js"
+  },
+  {
+    "revision": "51ff959a3c9ff0168fb2",
+    "url": "/static/js/runtime-main.ed8b82e0.js"
+  },
+  {
+    "revision": "6975ea7fd41ff1aad79fc0f0e0813f73",
+    "url": "/static/media/Teko.6975ea7f.ttf"
+  },
+  {
+    "revision": "59588b69bfc337a5e698917f66af7770",
+    "url": "/static/media/background.59588b69.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
